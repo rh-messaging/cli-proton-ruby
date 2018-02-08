9c4bcc677a58bc45fa5b05ba3e0c563c05cbe72c
Jiri Danek <jdanek@redhat.com>
2018-02-08 15:55:23 +0100

    NEW(ci) broker with AMQPS running as Docker Swarm Mode service

--- scripts/docker-tests.sh
+++ scripts/docker-tests.sh
@@ -3,7 +3,12 @@
 # project directory is mounted under /mnt
 PROJECT="/mnt"
 
+yum install -y centos-release-scl
+yum install -y rh-ruby24 rh-ruby24-ruby-devel
+source scl_source enable rh-ruby24
+
+cd /
 gem install minitest
-gem install qpid-proton/proton-c/bindings/ruby/gem/qpid_proton-0.21.0.gem
+gem install qpid-proton/proton-c/bindings/ruby/gem/qpid_proton-*.gem
 cd $PROJECT/tests
 ruby unit_tests.rb
