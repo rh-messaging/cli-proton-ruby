52b44fb3e64b458aef5e9d27d4e5a2d08db0678e
Radim Kubis <rkubis@redhat.com>
2018-02-26 21:15:54 +0000

    FIX(units): Fixed names of test methods to reflect rename/move of Utils to StringUtils

--- tests/unit/utils/unit_tests_string_utils.rb
+++ tests/unit/utils/unit_tests_string_utils.rb
@@ -22,126 +22,126 @@ require_relative '../../../lib/utils/string_utils'
 # StringUtils unit tests class
 class UnitTestsStringUtils < Minitest::Test
 
-  # is_int
-  def test_string_utils_is_int_true_in_string
+  # str_is_int
+  def test_string_utils_str_is_int_true_in_string
     refute StringUtils.str_is_int?("true")
   end
 
-  def test_string_utils_is_int_false_in_string
+  def test_string_utils_str_is_int_false_in_string
     refute StringUtils.str_is_int?("false")
   end
 
-  def test_string_utils_is_int_positive_int_in_string
+  def test_string_utils_str_is_int_positive_int_in_string
     assert StringUtils.str_is_int?("1")
   end
 
-  def test_string_utils_is_int_negative_int_in_string
+  def test_string_utils_str_is_int_negative_int_in_string
     assert StringUtils.str_is_int?("-1")
   end
 
-  def test_string_utils_is_int_positive_float_in_string
+  def test_string_utils_str_is_int_positive_float_in_string
     refute StringUtils.str_is_int?("1.3")
   end
 
-  def test_string_utils_is_int_negative_float_in_string
+  def test_string_utils_str_is_int_negative_float_in_string
     refute StringUtils.str_is_int?("-1.3")
   end
 
-  def test_string_utils_is_int_range_in_string
+  def test_string_utils_str_is_int_range_in_string
     refute StringUtils.str_is_int?("1..10")
   end
 
-  def test_string_utils_is_int_empty_array_in_string
+  def test_string_utils_str_is_int_empty_array_in_string
     refute StringUtils.str_is_int?("[]")
   end
 
-  def test_string_utils_is_int_simple_array_in_string
+  def test_string_utils_str_is_int_simple_array_in_string
     refute StringUtils.str_is_int?("['A', 'B', 'C']")
   end
 
-  def test_string_utils_is_int_empty_hash_in_string
+  def test_string_utils_str_is_int_empty_hash_in_string
     refute StringUtils.str_is_int?("{}")
   end
 
-  def test_string_utils_is_int_simple_hash_in_string
+  def test_string_utils_str_is_int_simple_hash_in_string
     refute StringUtils.str_is_int?("{1=>'A', 2=>'B', 3=>'C'}")
   end
 
-  def test_string_utils_is_int_empty_string_in_string
+  def test_string_utils_str_is_int_empty_string_in_string
     refute StringUtils.str_is_int?("")
   end
 
-  def test_string_utils_is_int_simple_string_in_string
+  def test_string_utils_str_is_int_simple_string_in_string
     refute StringUtils.str_is_int?("string")
   end
 
-  def test_string_utils_is_int_symbol_in_string
+  def test_string_utils_str_is_int_symbol_in_string
     refute StringUtils.str_is_int?(":ruby_symbol")
   end
 
-  def test_string_utils_is_int_nil_in_string
+  def test_string_utils_str_is_int_nil_in_string
     refute StringUtils.str_is_int?("nil")
   end
 
-  # is_float
+  # str_is_float
 
-  def test_string_utils_is_float_true_in_string
+  def test_string_utils_str_is_float_true_in_string
     refute StringUtils.str_is_float?("true")
   end
 
-  def test_string_utils_is_float_false_in_string
+  def test_string_utils_str_is_float_false_in_string
     refute StringUtils.str_is_float?("false")
   end
 
-  def test_string_utils_is_float_positive_int_in_string
+  def test_string_utils_str_is_float_positive_int_in_string
     assert StringUtils.str_is_float?("1")
   end
 
-  def test_string_utils_is_float_negative_int_in_string
+  def test_string_utils_str_is_float_negative_int_in_string
     assert StringUtils.str_is_float?("-1")
   end
 
-  def test_string_utils_is_float_positive_float_in_string
+  def test_string_utils_str_is_float_positive_float_in_string
     assert StringUtils.str_is_float?("1.3")
   end
 
-  def test_string_utils_is_float_negative_float_in_string
+  def test_string_utils_str_is_float_negative_float_in_string
     assert StringUtils.str_is_float?("-1.3")
   end
 
-  def test_string_utils_is_float_range_in_string
+  def test_string_utils_str_is_float_range_in_string
     refute StringUtils.str_is_float?("1..10")
   end
 
-  def test_string_utils_is_float_empty_array_in_string
+  def test_string_utils_str_is_float_empty_array_in_string
     refute StringUtils.str_is_float?("[]")
   end
 
-  def test_string_utils_is_float_simple_array_in_string
+  def test_string_utils_str_is_float_simple_array_in_string
     refute StringUtils.str_is_float?("['A', 'B', 'C']")
   end
 
-  def test_string_utils_is_float_empty_hash_in_string
+  def test_string_utils_str_is_float_empty_hash_in_string
     refute StringUtils.str_is_float?("{}")
   end
 
-  def test_string_utils_is_float_simple_hash_in_string
+  def test_string_utils_str_is_float_simple_hash_in_string
     refute StringUtils.str_is_float?("{1=>'A', 2=>'B', 3=>'C'}")
   end
 
-  def test_string_utils_is_float_empty_string_in_string
+  def test_string_utils_str_is_float_empty_string_in_string
     refute StringUtils.str_is_float?("")
   end
 
-  def test_string_utils_is_float_simple_string_in_string
+  def test_string_utils_str_is_float_simple_string_in_string
     refute StringUtils.str_is_float?("string")
   end
 
-  def test_string_utils_is_float_symbol_in_string
+  def test_string_utils_str_is_float_symbol_in_string
     refute StringUtils.str_is_float?(":ruby_symbol")
   end
 
-  def test_string_utils_is_float_nil_in_string
+  def test_string_utils_str_is_float_nil_in_string
     refute StringUtils.str_is_float?("nil")
   end
 
