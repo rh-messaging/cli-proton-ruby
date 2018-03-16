e017d61b971d72de0a9281939040218d66aea65a
Jiri Danek <jdanek@redhat.com>
2018-03-16 10:42:22 +0100

    NEW(options) --content-type sender option
    This closes #9

--- lib/defaults.rb
+++ lib/defaults.rb
@@ -27,6 +27,8 @@ module Defaults
   DEFAULT_MSG_PROPERTIES   = {}
   # Default value for message content
   DEFAULT_MSG_CONTENT      = nil
+  # Default value for content type
+  DEFAULT_CONTENT_TYPE     = "string"
   # Default value for message content type
   DEFAULT_MSG_CONTENT_TYPE = nil
   # Default message durability
