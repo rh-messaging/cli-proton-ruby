713d5c9013fb9e2f1aa5f3e379e2205cc75c1f5a
Jiri Danek <jdanek@redhat.com>
2018-09-12 16:37:24 +0200

    Fix docker-tests.sh to work with newly updated docker image

--- scripts/docker-tests.sh
+++ scripts/docker-tests.sh
@@ -3,14 +3,10 @@
 # project directory is mounted under /mnt
 PROJECT="/mnt"
 
-yum install -y centos-release-scl
-yum install -y rh-ruby24 rh-ruby24-ruby-devel
-source scl_source enable rh-ruby24
-
 cd /
 gem install rake
 gem install minitest
-gem install qpid-proton/proton-c/bindings/ruby/gem/qpid_proton-*.gem
+gem install qpid-proton/build/ruby/gem/qpid_proton-*.gem
 cd $PROJECT/tests
 rake unit_tests
 # rake client_tests
